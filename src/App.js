cbcb8d3629ad23c4a242690bcbac2cf2e79e1a83
Gaurav <aggarwalgaurav@gmail.com>
2021-09-06 16:11:33 +0530

    Working with components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 // import About from "./components/About";
 import NavBar from "./components/NavBar";
 import TextBox from "./components/TextBox";
+import React, { useState } from 'react';
+import Alert from "./components/Alert";
+
 function App() {
+  const [mode, setMode] = useState('light');
+  const toggleMode = ()=>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#1e0465';
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
   return (
     <>
-    <NavBar title="TextCleaner"/>
+    <NavBar title="TextCleaner" mode = {mode} toggleMode={toggleMode}/>
+    <strong><Alert alert=" Research says, Dark mode saves the moiture of your eyes, what are you waiting for?"/></strong>
     <div className="container my-3">
-      <TextBox heading="Enter the Text to Auto Analyse Below!!"/>
+      <TextBox heading="Enter the Text to Auto Analyse Below!!" mode={mode}/>
       {/* <About/> */}
     </div>
     </>
